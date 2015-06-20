9a096f10247b622680b4375c87c3f9d871a70c34
Gwen-Pigat <gwenael.pigat@gmail.com>
2015-06-20 19:15:42 +0200

    Refonte Bestial Soul

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<footer class="text-center">
+<a href="../index.php?contact"><h2>Contactez-nous</h2></a>
+
+<div class="socials">
+<i class="fa fa-facebook-square fa-4x"></i>
+<i class="fa fa-twitter-square fa-4x"></i>
+<i class="fa fa-youtube-square fa-4x"></i>
+</div>
+
+</footer>
---
+++ index.php
@@ -0,0 +1,317 @@
+<html>
+<head>
+	<?php include "include/header.php" ?>
+</head>
+<body>
+
+<?php if(isset($_GET) && isset($_GET['news'])){ ?>
+
+<title>Bestial Soul | News</title>
+
+<!-- Landing -->
+
+  	<div class="col-md-12 video text-center">
+  					<video autoplay loop no-controls muted>
+                        <source src="video-principale.mp4"  type="video/mp4" />
+                    </video>  
+  		<h1><span>N</span>ews </h1>
+	</div>
+
+
+<div class="container">
+
+<div class="col-md-8">
+<h2>Le groupe :</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<?php include "include/aside.php"; ?>
+<div class="col-md-8">
+<h2>Créateur du site :</h2>
+<img class="col-md-3 user" src="img/user.png" onmouseover="this.src='img/user.gif'" onmouseout="this.src='img/user.png'">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+</div>
+
+<?php }
+
+ elseif(isset($_GET) && isset($_GET['photos'])){ ?>
+
+<title>Bestial Soul | Photos</title>
+
+<!-- Landing -->
+
+  	<div class="col-md-12 text-center">
+  					<video autoplay loop no-controls muted>
+                        <source src="video-principale.mp4"  type="video/mp4" />
+                    </video>  
+  		<h1><span>P</span>hotos</h1>
+	</div>
+
+
+<div class="container">
+
+<div class="col-md-8">
+<h2>Le groupe :</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<?php include "include/aside.php"; ?>
+<div class="col-md-8">
+<h2><i class="fa fa-user fa-3x"></i> Créateur du site :</h2>
+<img class="col-md-3 user" src="img/user.png" onmouseover="this.src='img/user.gif'" onmouseout="this.src='img/user.png'">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+</div>
+
+<?php } 
+
+
+elseif(isset($_GET) && isset($_GET['videos'])){ ?>
+
+<title>Bestial Soul | Videos</title>
+
+<!-- Landing -->
+
+  	<div class="col-md-12 text-center">
+  					<video autoplay loop no-controls muted>
+                <source src="video-principale.mp4"  type="video/mp4" />
+            </video>  
+  		<h1><span>V</span>ideos</h1>
+	</div>
+
+
+<div class="container">
+
+<div class="col-md-8">
+<h2>Le groupe :</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<?php include "include/aside.php"; ?>
+<div class="col-md-8">
+<h2>Créateur du site :</h2>
+<img class="col-md-3 user" src="img/user.png" onmouseover="this.src='img/user.gif'" onmouseout="this.src='img/user.png'">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+</div>
+
+<?php }  
+
+
+elseif(isset($_GET) && isset($_GET['about_us'])){ ?>
+
+<title>Bestial Soul | A propos de nous</title>
+
+<!-- Landing -->
+
+  	<div class="col-md-12 text-center">
+  					<video autoplay loop no-controls muted>
+                        <source src="video-principale.mp4"  type="video/mp4" />
+                    </video>  
+  		<h1><span>B</span>estial <span>S</span>oul</h1>
+	</div>
+
+
+<div class="container">
+
+<div class="col-md-8">
+<h2>Le groupe :</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<?php include "include/aside.php"; ?>
+<div class="col-md-8">
+<h2>Créateur du site :</h2>
+<img class="col-md-3 user" src="img/user.png" onmouseover="this.src='img/user.gif'" onmouseout="this.src='img/user.png'">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+</div>
+
+<?php } 
+
+elseif(isset($_GET) && isset($_GET['contact'])){ ?>
+
+<title>Bestial Soul | Contact</title>
+
+<!-- Landing -->
+
+  	<div class="col-md-12 text-center">
+  					<video autoplay loop no-controls muted>
+                        <source src="video-principale.mp4"  type="video/mp4" />
+                    </video>  
+  		
+	</div>
+
+<div class="container">
+<form class="col-md-6 col-md-offset-3 contact" method="POST" action="index.php?envoi_message">
+  <center>
+  <h1 class="text-center"><span>C</span>ontact</h1>
+<input type="email" name="email" placeholder="Votre adresse e-mail" required><br>
+<textarea rows='5' cols='40' name="message" placeholder="Ecrivez vore message la-dedans :)" required></textarea><br>
+<button class="btn btn-danger"><i class="fa fa-envelope"></i> Envoyer</button><br>
+</center>
+</form>
+</div>
+
+<?php }
+
+
+elseif (isset($_GET) && isset($_GET['envoi_message'])) {
+    if (isset($_POST) && isset($_POST['email']) && isset($_POST['message']) && !empty($_POST['email']) && !empty($_POST['message'])) { ?>
+
+    <script type="text/javascript">
+    $(document).ready(function(){
+      alert("Message envoyé");
+    });
+    </script>
+    <!-- Landing -->
+
+    <div class="col-md-12 text-center">
+            <video autoplay loop no-controls muted>
+                        <source src="video-principale.mp4"  type="video/mp4" />
+                    </video>  
+      
+  </div>
+  <div class="container">
+<form class="col-md-6 col-md-offset-3 contact" method="POST" action="index.php?envoi_message">
+  <center>
+  <h1 class="text-center"><span>C</span>ontact</h1>
+<input type="email" name="email" placeholder="Votre adresse e-mail" required><br>
+<textarea rows='5' cols='40' name="message" placeholder="Ecrivez vore message la-dedans :)" required></textarea><br>
+<button class="btn btn-danger"><i class="fa fa-envelope"></i> Envoyer</button><br>
+</center>
+</form>
+</div>
+
+    <?php
+      require "PHPMailer/class.phpmailer.php";
+      
+  $local = "localhost";
+  $user = "root";
+  $password = "motdepasselocalhostgwen";  
+  $db_name = "BestialSoul";
+
+  extract($_POST);
+
+  $link = mysqli_connect("$local","$user","$password","$db_name")or die("Erreur lors de l'envoi de votre message");
+
+   mysqli_query($link, "INSERT INTO Contact(Email, Message) VALUES('$email','$message')") or die("Erreur");
+
+    $mail = new phpmailer();
+
+    $mail->FromName = "PixOFHeaven, A Bestial send you a message";
+    $mail->Subject = "$email - Formulaire de contact";
+    
+    $body = "la personne suivante à laissé un message.\n 
+    Email :         $email \n
+    Son message : $message\n\n\n\n\n
+
+    Cordialement, Le robot de PixOFHeaven.\n
+    ---------------------------------------\n
+    Ceci est un mail automatique, Merci de ne pas y répondre.";
+
+    $mail->Body = $body;
+
+    
+    // Add a recipient address
+    $mail->AddAddress('pixofheaven@gmail.com');
+
+    if(!$mail->Send())
+        echo ('');
+    else
+        echo ('');
+
+      header('Refresh: 0 ;url=index.php?contact');
+    }
+}
+
+else{ ?>
+
+<title>Bestial Soul | Accueil</title>
+
+<!-- Landing -->
+
+  	<div class="col-md-12 text-center">
+  					<video autoplay loop no-controls muted>
+                        <source src="video-principale.mp4"  type="video/mp4" />
+                        <h1>Test de titre</h1>
+                    </video>
+
+  		<h1><span>A</span>ccueil</h1>
+	</div>
+
+
+<div class="container">
+
+<div class="col-md-8">
+<h2>Le groupe :</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<?php include "include/aside.php"; ?>
+
+<div class="col-md-8">
+<h2>Créateur du site :</h2>
+<img class="col-md-3 user" src="img/user.png" onmouseover="this.src='img/user.gif'" onmouseout="this.src='img/user.png'">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+</div>
+
+<?php } 
+
+include "include/footer.php"; ?>
+
+</body>
+</html>	
